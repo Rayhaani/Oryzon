1fed35389617c65573fa248ab5db0dd803af6e98
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-03 19:56:31 +0100

    Reorder unloadPageOwnCss and scrollTo calls

--- router.js
+++ router.js
@@ -548,13 +548,13 @@
             }
 
             runDestroy(currentPath);
-           unloadPageOwnCss(currentPath);
             await Promise.all([
                 loadStylesheetsAll(PAGE_STYLES[targetPath]),
                 preloadScript ? loadScriptOnce(preloadScript) : Promise.resolve()
             ]).catch(e => console.error(e));
             currentContentEl.innerHTML = newContent.innerHTML;
-            window.scrollTo(0, 0);
+           unloadPageOwnCss(currentPath);
+           window.scrollTo(0, 0);
             if (newDoc.title) document.title = newDoc.title;
 
             // Update history + internal state.
