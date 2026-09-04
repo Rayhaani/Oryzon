bc955e9a0ca023bce5c0d93c0bf332e689b43f1b
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-04 17:30:45 +0100

    Fix import order in router.js

--- router.js
+++ router.js
@@ -219,7 +219,7 @@
             'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
             'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js',
             'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js',
-            'health.js'
+            'health.js',
              'footer.js'
         ],
     };
