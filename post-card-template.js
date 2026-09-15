d24192617b8eb91d56ca8d92bfafa972b97689e4
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-15 22:44:29 +0100

    Decrease font size of post timestamp

--- post-card-template.js
+++ post-card-template.js
@@ -208,7 +208,7 @@
 
         /* ===== TIMESTAMP ===== */
         .post-time {
-            font-size: 11.5px !important;
+            font-size: 10.5px !important;
             color: rgba(255, 255, 255, 0.45) !important;
             display: block !important;
             margin-top: 0 !important;
