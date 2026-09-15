4fdaa44d82dde64b7d91032f3b1175659d99e9f3
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-15 09:25:32 +0100

    Adjust padding-bottom in post card template

--- post-card-template.js
+++ post-card-template.js
@@ -371,7 +371,7 @@
     flex-direction: column !important;
     justify-content: flex-end !important;
     background: #000 !important;
-    padding-bottom: calc(var(--nx-footer-h, 50px) + 60px) !important;
+    padding-bottom: calc(var(--nx-footer-h, 50px) + 6px) !important;
 }
 
         .immersive-mode .post-media {
