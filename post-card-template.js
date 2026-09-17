56d84151dbd70bb2a9d9944094b95de2de50b87c
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-17 14:38:13 +0100

    Modify height and width of post card elements
    Adjusted height and dimensions of elements in post card template.

--- post-card-template.js
+++ post-card-template.js
@@ -146,7 +146,7 @@
             align-items: center !important;
             justify-content: space-between !important;
             padding: 0 15px 0 40px !important;
-            height: 46px !important;
+            height: 43px !important;
             position: relative !important;
             
             /* === Kawai wannan biyu aka canza === */
@@ -164,8 +164,8 @@
             left: 0px !important;
             top: 0px !important;
             transform: none !important;
-            width: 36px !important;
-            height: 36px !important;
+            width: 40px !important;
+            height: 40px !important;
             border-radius: 50% !important;
             border: 1px solid var(--premium-gold) !important;
             object-fit: cover !important;
