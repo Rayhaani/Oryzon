edd4361cc6b7d74ff801034e84ca0293b8fa27d3
Rayhaani <78444181+Rayhaani@users.noreply.github.com>
2026-09-17 14:12:18 +0100

    Increase dimensions of element to 36px

--- post-card-template.js
+++ post-card-template.js
@@ -164,8 +164,8 @@
             left: 0px !important;
             top: 0px !important;
             transform: none !important;
-            width: 34px !important;
-            height: 34px !important;
+            width: 36px !important;
+            height: 36px !important;
             border-radius: 50% !important;
             border: 1px solid var(--premium-gold) !important;
             object-fit: cover !important;
